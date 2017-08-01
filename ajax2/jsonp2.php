56b1180f81a05e7c4f7322b25d3d1a6026c1345d
Myecho <1482251673@qq.com>
2017-08-01 20:15:32 +0800

    ajax

--- ajax2/jsonp2.php
+++ ajax2/jsonp2.php
@@ -2,6 +2,7 @@
 //这个文件是远程端：http://www.li.cc/jsonp.php
 $_arr=array('a'=>1,'b'=>2,'c'=>3);
 $_result=json_encode($_arr);
-echo $_result;
+$_callback=$_GET['callback'];
+echo $_callback."($_result)";
 ?>
 
