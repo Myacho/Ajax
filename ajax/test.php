d8148d7590a80d8c11581c85880466ef9bcb7acd
Myecho <1482251673@qq.com>
2017-08-01 16:37:07 +0800

    ajax of jquery

---
+++ ajax/test.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+	if($_GET['url']=='www.yck.com'){
+		echo"瓢虫web俱乐部！";
+	}else{
+		echo"不存在的！";
+	}
+	*/
+	if($_POST['url']=='www.yck.com'){
+		echo"瓢虫web俱乐部！";
+	}else{
+		echo"不存在的！";
+	}
+
+?>
